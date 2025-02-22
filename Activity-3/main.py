d50a76e2836b03d868657f8d9ce92c626a05f671
TomiwaMartins21 <martinstomiwa838@gmail.com>
2025-02-22 13:40:51 +0100

    Initial commit

---
+++ Activity-3/main.py
@@ -0,0 +1,4 @@
+# Python program to calculate the sum of the first ten natural numbers
+# Use bulit-in sum and range functions to caluculate the sum 
+total_sum = sum(range (1, 11))
+print(f"The sum of the first ten nautral numbers is {total_sum}")
